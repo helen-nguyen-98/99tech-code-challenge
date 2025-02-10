eacf92158928aa0582bccf1147891012f5ef146e
Helen Nguyen <nt.hau@tego.global>
2025-02-10 17:17:24 +0700

    add description for problem 1

--- src/problem1/solution.js
+++ src/problem1/solution.js
@@ -1,6 +1,12 @@
 // we have 2 cases for sum_to_n
 // 1. positive n
 // 2. negative n
+// based on the example we can see that the sum of 1 to 5 is 15
+// so I assume the requirement is to sum all the numbers from 1 to n
+// we can use the formula (n * (n + 1)) / 2 to calculate the sum of numbers from 1 to n
+// but n is integer so we need to consider the negative numbers as well
+// if n is negative we need to sum all the numbers from -n to -1 
+// we can use the same formula but we need to return the result as negative
 
 var sum_to_n_a = function (n) {
   if (n === 0) return 0;
